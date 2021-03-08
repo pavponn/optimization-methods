e698164a150b128aeed6c68819c2e245495dcb73
Pavel Ponomarev <pavponn@yandex.ru>
2021-03-08 11:10:50 +0300

    Minor renaming

--- lab-1/src/one_dim_search.py
+++ lab-1/src/one_dim_search.py
@@ -7,32 +7,32 @@ GOLDEN_RATION_CONSTANT = (1 + math.sqrt(5)) / 2
 
 
 def dichotomy_method(f, a, b, eps=DEFAULT_EPSILON, max_iter=DEFAULT_MAX_ITERATIONS):
-    a_i, b_i = a, b
+    a_k, b_k = a, b
     delta = eps / 2 - 1e-10
     iters = 0
-    while abs(a_i - b_i) >= eps and iters < max_iter:
+    while abs(a_k - b_k) >= eps and iters < max_iter:
         iters += 1
-        x_1 = (a_i + b_i) / 2.0 - delta
-        x_2 = (a_i + b_i) / 2.0 + delta
+        x_1 = (a_k + b_k) / 2.0 - delta
+        x_2 = (a_k + b_k) / 2.0 + delta
         f_1 = f(x_1)
         f_2 = f(x_2)
         if f_1 >= f_2:
-            a_i = x_1
+            a_k = x_1
         else:
-            b_i = x_2
+            b_k = x_2
 
-    return (a_i + b_i) / 2.00, iters
+    return (a_k + b_k) / 2.00, iters
 
 
 def golden_selection_method(f, a, b, eps=DEFAULT_EPSILON, max_iter=DEFAULT_MAX_ITERATIONS):
-    a_i, b_i = a, b
-    x_1 = b_i - abs(b_i - a_i) / GOLDEN_RATION_CONSTANT
-    x_2 = a_i + abs(b_i - a_i) / GOLDEN_RATION_CONSTANT
+    a_k, b_k = a, b
+    x_1 = b_k - abs(b_k - a_k) / GOLDEN_RATION_CONSTANT
+    x_2 = a_k + abs(b_k - a_k) / GOLDEN_RATION_CONSTANT
     f_prev = f(x_1)
     use_x_1 = True
     iters = 0
 
-    while abs(b_i - a_i) >= eps and iters < max_iter:
+    while abs(b_k - a_k) >= eps and iters < max_iter:
         iters += 1
         if use_x_1:
             f_2 = f(x_2)
@@ -42,33 +42,31 @@ def golden_selection_method(f, a, b, eps=DEFAULT_EPSILON, max_iter=DEFAULT_MAX_I
             f_2 = f_prev
 
         if f_1 >= f_2:
-            a_i = x_1
+            a_k = x_1
             x_1 = x_2
-            x_2 = a_i + abs(b_i - a_i) / GOLDEN_RATION_CONSTANT
+            x_2 = a_k + abs(b_k - a_k) / GOLDEN_RATION_CONSTANT
             f_prev = f_2
             use_x_1 = True
         else:
-            b_i = x_2
+            b_k = x_2
             x_2 = x_1
-            x_1 = b_i - abs(b_i - a_i) / GOLDEN_RATION_CONSTANT
+            x_1 = b_k - abs(b_k - a_k) / GOLDEN_RATION_CONSTANT
             use_x_1 = False
             f_prev = f_1
 
-    return (b_i + a_i) / 2, iters
+    return (b_k + a_k) / 2, iters
 
 
 def fibonacci_method(f, a, b, eps=DEFAULT_EPSILON):
     a_k, b_k = a, b
     n, fibs = get_n_and_fibs(a, b, eps)
-    # for index
-    n = n - 1
+    n = n - 1  # for indexing
     k = 1
     x_1 = a_k + fibs[n] / fibs[n + 2] * (b_k - a_k)
     x_2 = a_k + fibs[n + 1] / fibs[n + 2] * (b_k - a_k)
     f_prev = f(x_1)
     use_x_1 = True
     while k < n:
-        # print(f'x1: {x_1}, x2: {x_2}, length: {abs(x_2 - x_1)}')
         if use_x_1:
             f_2 = f(x_2)
             f_1 = f_prev
@@ -108,4 +106,3 @@ def generate_fib(a, b, eps):
             break
         k += 1
     return fibs
-
