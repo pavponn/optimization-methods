6f442f5a7b02c15f25402d7510f9f9aa78f20022
Pavel Ponomarev <pavponn@yandex.ru>
2021-03-08 11:27:30 +0300

    Restructure project

--- lab1/test/one_dim_search_test.py
+++ lab1/test/one_dim_search_test.py
@@ -1,7 +1,7 @@
 import unittest
 from parameterized import parameterized, parameterized_class
 
-from src.one_dim_search import (
+from lab1.src.one_dim_search import (
     dichotomy_method,
     golden_selection_method,
     fibonacci_method
