3b146c2e15033c0b459c26fe885be18dee9453ce
Ilya Bizyaev <bizyaev@zoho.com>
2021-04-03 10:56:58 +0300

    Add RAM and time measurement

--- lab2/src/comparison/compare_performance.py
+++ lab2/src/comparison/compare_performance.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from memory_profiler import memory_usage
 
 from lab1.src.grad.grad_step_strategy import StepStrategy
 from lab1.src.grad.grad_descent import gradient_descent
@@ -6,23 +8,26 @@ from lab1.src.grad.stop_criteria import StopCriteria
 
 from lab2.src.methods.newton_method import newton_method
 from lab2.src.methods.conjugate_method import conjugate_direction_method
-from lab2.src.comparison.compare_methods import (
-    quadratic_function,
-    quadratic_function_grad,
-    quadratic_function_hess,
-)
 
 
 EPS = 1e-9
 
 FUNCTIONS = [
     (
-        "100(y - x)² + (1 - x)²",  # f_str
-        quadratic_function,  # f
-        quadratic_function_grad,  # f_grad
-        quadratic_function_hess,  # f_hess
-        np.array([-2, 0], dtype="float64"),  # b
-        (1e12, -1e12),  # start
+        "100(y - x)² + (1 - x)² + (z - y)² + 5(x + y)²",  # f_str
+        lambda x: 100 * (x[1] - x[0]) ** 2 + (1 - x[0]) ** 2 + (x[2] - x[1]) ** 2 + 5 * (x[0] + x[1]) ** 2,  # f
+        lambda x: np.array([
+            212 * x[0] - 190 * x[1] - 2,
+            -190 * x[0] + 212 * x[1] - 2 * x[2],
+            -2 * x[1] + 2 * x[2]
+        ], dtype="float64"),  # f_grad
+        lambda x: np.array([
+                [212, -190, 0],
+                [-190, 212, -2],
+                [0, -2, 2]
+            ], dtype="float64"),  # f_hess
+        np.array([-2, 0, 0], dtype="float64"),  # b
+        (1e8, -1e8, 2 * 1e7),  # start
     )
 ]
 
@@ -59,12 +64,24 @@ METHODS = {
 }
 
 
+def plot_memory_usage(method_name: str, mem_usage: [float]):
+    print(f"Peak memory usage: {max(mem_usage):.2f} MiB")
+    plt.plot([i * 0.05 for i in range(len(mem_usage))], mem_usage)
+    plt.title(f"Memory usage by {method_name} (MiB)")
+    plt.show()
+
+
 def main():
     for f_str, f, f_grad, f_hess, b, start in FUNCTIONS:
         for (method_name, method) in METHODS.items():
-            print(f"Running {method_name}...")
-            iters = method(f, f_grad, f_hess, b, start)
+            print(f"# Running {method_name}...")
+            mem_usage, iters = memory_usage(
+                (method, (f, f_grad, f_hess, b, start)),
+                interval=0.05,
+                retval=True
+            )
             print(f"Finished in {iters} iterations")
+            plot_memory_usage(method_name, mem_usage)
 
 
 if __name__ == '__main__':
