e2f02d32f19e00f965780f4b0ca5c50319717d41
jariklag <jarikdorohov@gmail.com>
2021-04-07 23:07:19 +0300

    added simplex method and tests

---
+++ lab3/src/methods/simplex_method.py
@@ -0,0 +1,195 @@
+import math
+from typing import Optional, Tuple, Union
+
+import numpy as np
+
+"""
+Simple simplex method from: 
+    http://www.itlab.unn.ru/uploads/opt/optBook1.pdf
+"""
+
+EPS = 1e-15
+
+
+def gauss_step(A, r, s):
+    n, m = A.shape
+    qrs = A[r, s]
+    for j in range(m):
+        A[r, j] /= qrs
+    for i in range(n):
+        if r == i:
+            continue
+        t = A[i, s]
+        for j in range(m):
+            A[i, j] -= A[r, j] * t
+
+
+def core(A, B) -> bool:
+    """
+    A = ( c )
+        (b|A)
+    Presume: b[i] >= 0
+    B - basis
+    :return isSystem has bound solution
+    """
+    assert np.all(A[1:, [0]] >= -EPS)
+
+    n, m = A.shape
+    while True:
+        s = None
+        for j in range(1, m):
+            if A[0, j] < 0:
+                s = j
+                break
+        if s is None:
+            break
+
+        r, t = None, math.inf
+        for i in range(1, n):
+            if A[i, s] > 0 and A[i, 0] / A[i, s] < t:
+                r = i
+                t = A[i, 0] / A[i, s]
+        if r is None:
+            return False
+
+        # Gauss step
+        gauss_step(A, r, s)
+
+        B[r - 1] = s
+    return True
+
+
+def _simplex_method(A, b, c) -> Optional[Tuple[float, np.ndarray, np.ndarray]]:
+    """
+    Presume:
+    forall i, b[i] >= 0
+    A, b, c : np.ndarray
+    Ax = b
+    cx -> max
+    :return None or (maximum, result_x, Basis)
+    """
+    assert np.all(b >= -EPS)
+    m, n = A.shape
+
+    # first phase
+    ## intros fake variables
+    A = np.hstack((A, np.eye(len(A))))
+
+    ## intro new function
+    """
+    f = max(-x[n+1] - ... -x[n+m])
+    """
+    # add top row (0,0,0,...x[n+1], x[n+2]... x[n+m]) and left row (0, b[0], b[1]...b[n])
+    z = np.concatenate((np.zeros(1), np.zeros(n), np.repeat(1, m)))
+    Bz = np.arange(n + 1, n + m + 1)
+    A = np.hstack((b.reshape((len(b), 1)), A))
+    A = np.vstack((z.reshape((1, len(z))), A))
+
+    # get rid of 1 in fist row
+    # row[0] -= sum(row[1..m])
+    for i in range(1, m + 1):
+        A[0] = A[0] - A[i]  # equal gauss step for basic vector
+
+    if not core(A, Bz):
+        return None
+    if A[0, 0] < -EPS:
+        return None
+    # set invariant B_z
+    for b_ind, s in enumerate(Bz):
+        if n < s:
+            r = None
+            for i in range(1, m + 1):
+                if r is None and A[i, s] == 1:
+                    r = i
+                else:
+                    assert A[i, s] == 0, "in column more that 1 not zero element"
+            assert r is not None, "0-base vector"
+
+            k = None
+            for i in range(1, n + 1):
+                if A[r, i] != 0:
+                    k = i
+                    break
+            if k is None:
+                A = np.delete(A, r, 0)
+                Bz = np.delete(Bz, r - 1)
+            else:
+                gauss_step(A, r, k)
+                Bz[b_ind] = k
+
+    c = np.concatenate((np.zeros(1), -c))
+    A = np.vstack((c.reshape((1, len(c))), A[1:, 0:n + 1]))
+
+    for i, s in enumerate(Bz, start=1):
+        if A[0, s] != 0:
+            assert A[i, s] == 1
+            A[0] -= A[i] * A[0, s]
+
+    first_opt_solution = np.zeros(n)
+    for i, s in enumerate(Bz, start=1):
+        first_opt_solution[s - 1] = A[i, 0]
+
+    # second phase
+    if not core(A, Bz):
+        return None
+    solution = np.zeros(n)
+    for i, s in enumerate(Bz, start=1):
+        solution[s - 1] = A[i, 0]
+    res = A[0, 0]
+    assert len(Bz) == m
+    return res, solution, Bz
+
+
+def simplex_method(A, b, c, leq=False, dual=False) -> Optional[Union[Tuple[float, np.ndarray],
+                                                                     Tuple[float, np.ndarray, np.ndarray]]]:
+    """
+    <x, c> -> max
+    if leq:
+        Ax <= b
+    else:
+        Ax == b
+    """
+    if isinstance(A, list):
+        A = np.array(A)
+    if isinstance(b, list):
+        b = np.array(b)
+    if isinstance(c, list):
+        c = np.array(c)
+
+    if leq:
+        A = np.hstack((A, np.eye(len(b))))
+        c = np.hstack((c, np.zeros(len(b))))
+
+    for i in range(len(b)):
+        if b[i] < 0:
+            b[i] *= -1
+            A[i] *= -1
+
+    t = _simplex_method(A, b, c)
+    if t is None:
+        return None
+    maximum, sol, Bz = t
+
+    if leq:
+        res = maximum, sol[:-len(b)]
+    else:
+        res = maximum, sol
+
+    if dual:  # experimental feature
+        A_dual = A[:, Bz]
+        c_dual = c[Bz]
+        sol_dual = c_dual.dot(np.linalg.inv(A_dual))
+        res = res[0], res[1], sol_dual
+
+    return res
+
+
+def main():
+    A = np.array([[1, 1, -1, 3],
+                  [1, -2, 3, -1],
+                  [5, -4, 7, 3]])
+    b = np.array([1, 1, 5])
+    c = np.array([-1, -1, 0, -5])
+    print(f'Sample:\nA:\n{A}\nb:{b}, c:{c}\n')
+    res, x = simplex_method(A, b, c, leq=False)
+    print(f'Maximum: {res} on {x}')
---
+++ lab3/test/test_simplex_method.py
@@ -0,0 +1,95 @@
+import unittest
+import scipy.optimize as opt
+import numpy as np
+from parameterized import parameterized
+from lab3.src.methods.simplex_method import simplex_method
+
+simplex_method_testcases = [
+    (
+        np.array([[1, 2, -1, 2, 4],
+                  [0, -1, 2, 1, 3],
+                  [1, -3, 2, 2, 0]]),
+        np.array([1, 3, 4]),
+        np.array([1, -3, 2, 1, 4]),
+    ), (
+        np.array([[1, 3, 0, 2, 1],
+                  [2, -1, 1, 2, 3],
+                  [1, -1, 2, 1, 0]]),
+        np.array([1, 2, 4]),
+        np.array([-1, -3, 2, 1, 4]),
+    ), (
+        np.array([[-1, 3, 0, 2, 1],
+                  [2, -1, 1, 2, 3],
+                  [1, -1, 2, 1, 0]]),
+        np.array([1, 4, 5]),
+        np.array([-1, 0, -2, 5, 4]),
+    ), (
+        np.array([[2, 3, 1, 2, 1],
+                  [2, 1, -3, 2, 1],
+                  [2, 1, 2, 1, 0]]),
+        np.array([1, 3, 1]),
+        np.array([-1, 1, -2, 1, 5]),
+    ), (
+        np.array([[2, 1, 3, 4],
+                  [1, -1, 2, 1],
+                  [0, 0, 1, 3]]),
+        np.array([2, 4, 1]),
+        np.array([-2, 3, 4, -1]),
+    ), (
+        np.array([[2, 3, 1, 2],
+                  [2, -1, 2, 1],
+                  [1, 1, 0, -1]]),
+        np.array([3, 4, 1]),
+        np.array([-2, 3, -3, 3]),
+    ), (
+        np.array([[2, 3, -1, 2],
+                  [1, 1, 1, 1],
+                  [2, -1, 0, 2]]),
+        np.array([1, 1, 2]),
+        np.array([-2, 3, 4, -1]),
+    ), (
+        np.array([[2, 1, 3, 4],
+                  [2, -1, 2, 1],
+                  [0, 0, 1, 2]]),
+        np.array([1, 2, 4]),
+        np.array([-2, 3, 4, -1]),
+    ), (
+        np.array([[1, 2, 3, 1, 2, 5],
+                  [2, -3, 1, 2, 1, 4]]),
+        np.array([1, 2]),
+        np.array([-2, 3, 4, -1, 2, 1]),
+    ), (
+        np.array([[3, 2, 1, -3, 2, 1],
+                  [1, 1, 0, 0, 1, 1]]),
+        np.array([3, 2]),
+        np.array([-2, 3, 1, 2, 0, 1]),
+    ), (
+        np.array([[1, 2, 3, 4, 5, 6],
+                  [2, 1, -3, 2, 1, -3]]),
+        np.array([1, 4]),
+        np.array([1, -1, 2, 3, 1, 0]),
+    ), (
+        np.array([[2, 3, -1, 0, 2, 1],
+                  [2, 0, 3, 0, 1, 1]]),
+        np.array([1, 2]),
+        np.array([-2, 3, 4, -1, 2, 1]),
+    )
+]
+
+DELTA = 9
+
+
+class TestNewtonMethod(unittest.TestCase):
+    @parameterized.expand(simplex_method_testcases)
+    def test_newton_method(self, A, b, c):
+        t = simplex_method(A, b, c)
+        result = t if t is not None else (None, None)
+        expected_result = opt.linprog(c=-c, A_eq=A, b_eq=b, method='simplex')
+        self.check_result(result, expected_result)
+
+    def check_result(self, result, expected_result):
+        res, x = result
+        if not expected_result.success:
+            self.assertIsNone(x)
+        else:
+            np.testing.assert_array_almost_equal(res, -expected_result.fun, DELTA)
